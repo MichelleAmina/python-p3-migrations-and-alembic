adcfa58ecd75ebb60f9888b16765658d4d56dd76
Michelle Amina Okumu <michelleaokumu@gmail.com>
2024-03-15 18:47:22 +0300

    Completed code-along

---
+++ lib/migrations/versions/b1a6e2b0a5af_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: b1a6e2b0a5af
+Revises: 
+Create Date: 2024-03-15 18:24:22.288541
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b1a6e2b0a5af'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
